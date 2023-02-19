14961035cf1f3040b84e252796bf1b6119b58883
Diego-Fdez <dieghoxcr7@gmail.com>
2023-02-19 15:17:33 -0600

    add connection to db

--- src/db/database.js
+++ src/db/database.js
@@ -1,10 +1,27 @@
-import { Sequelize } from "sequelize";
+import { Sequelize } from 'sequelize';
+import dotenv from 'dotenv';
 
-export const sequelize = new Sequelize('burbujasDeAlgodonDB', 'root', 'password', {
-  host: 'localhost',
-  dialect: 'mysql' /* one of 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */
-})
+dotenv.config();
 
+export const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: 'localhost',
+    dialect: 'mysql', // one of 'mysql',
+  }
+);
+
+export const dbConnection = async () => {
+  //starting connection
+  try {
+    await sequelize.sync({ force: false });
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+};
 
 /*
 que la base de dato conecte deben crear la base de datos en gestor de base de daros
